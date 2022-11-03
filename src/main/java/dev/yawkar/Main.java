b9c60ecc7485e45d40117bc8f634fb1f56df4513
yawkar <yawkarpov@gmail.com>
2022-11-03 20:20:45 +0300

    feat: add the final algorithm and opencsv for writing results & results themselves
    Algorithm explanation:
    We use 2 execution services - one for pulling HTMLs from URLs and the other one for parsing actual data.
    First of all, we get the number of cryptocurrencies in the Yahoo!Finance list.
    After we got it we pull 100 cryptocurrencies per page every 5 seconds (with proxy we can make it in a moment) and create CompletableFuture that parses the pulled Document, populates Cryptocurrency instances and adds them into ConcurrentLinkedQueue.
    
    After all tasks complete we write results down into csv file that has name in the following format:
    "results-YYYY-MM-DDThh:mm:ss.SSSSSSSSS"

--- src/main/java/dev/yawkar/Main.java
+++ src/main/java/dev/yawkar/Main.java
@@ -1,7 +1,62 @@
 package dev.yawkar;
 
+import com.opencsv.CSVWriter;
+import dev.yawkar.crawler.JSoupCrawler;
+import dev.yawkar.model.Cryptocurrency;
+import dev.yawkar.strategy.impl.ParseCryptocurrenciesStrategy;
+import dev.yawkar.task.ParseTask;
+import dev.yawkar.util.YahooFinanceHelper;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Queue;
+import java.util.concurrent.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // using SingleThreadExecutor because I don't have proxies
+        var crawlExecutor = Executors.newSingleThreadExecutor();
+        var parseExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        Queue<Cryptocurrency> cryptocurrencies = new ConcurrentLinkedQueue<>();
+
+        int numberOfCryptocurrenciesToParse = YahooFinanceHelper.getNumberOfCryptocurrencies();
+        for (int hundredIndex = 0; hundredIndex < numberOfCryptocurrenciesToParse / 100; ++hundredIndex) {
+            String targetUrl = "https://finance.yahoo.com/crypto?count=%d&offset=%d".formatted(100, hundredIndex * 100);
+            // if you have proxies, you can rewrite it as JSoupCrawler.of(targetUrl, yourProxy).call() here and then remove Thread.sleep(5000) in the bottom
+            CompletableFuture
+                    .supplyAsync(() -> JSoupCrawler.of(targetUrl).call(), crawlExecutor)
+                    .thenAcceptAsync(doc -> ParseTask.of(doc, new ParseCryptocurrenciesStrategy(), cryptocurrencies::add).run(), parseExecutor);
+            // we're running without any proxy, therefore, we can be banned for too many requests in a short period of time
+            Thread.sleep(5000);
+        }
+
+        int remainingCryptocurrencies = numberOfCryptocurrenciesToParse % 100;
+        if (remainingCryptocurrencies > 0) {
+            String targetUrl = "https://finance.yahoo.com/crypto?count=%d&offset=%d"
+                    .formatted(remainingCryptocurrencies, numberOfCryptocurrenciesToParse / 100);
+            CompletableFuture
+                    .supplyAsync(() -> JSoupCrawler.of(targetUrl).call(), crawlExecutor)
+                    .thenAcceptAsync(doc -> ParseTask.of(doc, new ParseCryptocurrenciesStrategy(), cryptocurrencies::add).run(), parseExecutor);
+        }
+
+        crawlExecutor.shutdown();
+        while (!crawlExecutor.awaitTermination(5, TimeUnit.SECONDS));
+        parseExecutor.shutdown();
+        while (!parseExecutor.awaitTermination(5, TimeUnit.SECONDS));
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter("results-" + LocalDateTime.now() + ".csv"))) {
+            writer.writeNext(new String[]{"Symbol", "Name", "Price", "Change", "Market Cap", "Circulating Supply"});
+            for (var cryptocurrency : cryptocurrencies) {
+                writer.writeNext(new String[]{
+                        cryptocurrency.symbol(),
+                        cryptocurrency.name(),
+                        cryptocurrency.price().toPlainString(),
+                        cryptocurrency.change().toPlainString(),
+                        cryptocurrency.marketCap().toPlainString(),
+                        cryptocurrency.circulatingSupply().toPlainString()
+                });
+            }
+        }
     }
 }
